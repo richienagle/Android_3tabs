1573a843db76aa240e269cf3d79eb556cb85c253
richienagle <richienagle@yahoo.com>
2019-01-18 20:21:58 -0800

    Initial commit

---
+++ app/src/main/java/com/example/richie/a3tabs/Tab1Connect.java
@@ -0,0 +1,17 @@
+package com.example.richie.a3tabs;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Connect extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1connect, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/richie/a3tabs/Tab2Locate.java
@@ -0,0 +1,17 @@
+package com.example.richie.a3tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Locate extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2locate, container, false);
+        return rootView;
+    }
+}
